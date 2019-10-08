a9706d922ba3eb90bc9b9ceee1b19110cb31f610
taki-d <taka410199@gmail.com>
2019-10-09 00:17:02 +0900

    [add]add floating action button

--- app/src/main/java/net/orangepla/todoapp/TaskViewAdapter.kt
+++ app/src/main/java/net/orangepla/todoapp/TaskViewAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import java.text.SimpleDateFormat
 
 class TaskViewAdapter(private val context: Context, private val taskList: List<TaskModel>) :
         RecyclerView.Adapter<TaskViewAdapter.TaskViewHolder>() {
@@ -22,8 +23,10 @@ class TaskViewAdapter(private val context: Context, private val taskList: List<T
         override fun getItemCount(): Int = taskList.size
 
         override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+                val dateFormatter = SimpleDateFormat("MM/dd HH:mm")
+
                 holder.titleTextView.text = taskList[position].title
-                holder.dateTextView.text = taskList[position].date.toString()
+                holder.dateTextView.text = dateFormatter.format(taskList[position].date)
         }
 }
 
